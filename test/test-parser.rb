1c16fc3251dc6f64dc759d51bb7d54aa76d513fb
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-05-04 01:34:12 +0900

    Support tie

--- test/test-parser.rb
+++ test/test-parser.rb
@@ -32,4 +32,44 @@ class TestParser < Test::Unit::TestCase
                  ],
                  parser.parse)
   end
+
+  def test_a_tie
+    parser = Rhythmmml::Parser.new("c&c", 8000, bpm: 60)
+    assert_equal([
+                   ["c", 2.0, [523.252, 8000, 2.0, 0.5]],
+                 ],
+                 parser.parse)
+  end
+
+  def test_a_tie_with_length
+    parser = Rhythmmml::Parser.new("c&c16", 8000, bpm: 60)
+    assert_equal([
+                   ["c", 1.25, [523.252, 8000, 1.25, 0.5]],
+                 ],
+                 parser.parse)
+  end
+
+  def test_a_tie_with_dot
+    parser = Rhythmmml::Parser.new("c&c.", 8000, bpm: 60)
+    assert_equal([
+                   ["c", 2.5, [523.252, 8000, 2.5, 0.5]],
+                 ],
+                 parser.parse)
+  end
+
+  def test_a_tie_with_length_with_dot
+    parser = Rhythmmml::Parser.new("c&c2.", 8000, bpm: 60)
+    assert_equal([
+                   ["c", 4.0, [523.252, 8000, 4.0, 0.5]],
+                 ],
+                 parser.parse)
+  end
+
+  def test_ties
+    parser = Rhythmmml::Parser.new("c&c&c", 8000, bpm: 60)
+    assert_equal([
+                   ["c", 3.0, [523.252, 8000, 3.0, 0.5]],
+                 ],
+                 parser.parse)
+  end
 end
